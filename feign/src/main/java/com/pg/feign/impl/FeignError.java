8a283909cac44981ca8439d9bb2b56cb7746df39
pangen <1766739122@qq.com>
2021-10-30 22:13:47 +0800

    springcloud

---
+++ feign/src/main/java/com/pg/feign/impl/FeignError.java
@@ -0,0 +1,20 @@
+package com.pg.feign.impl;
+
+import com.pg.entity.Student;
+import com.pg.feign.FeignProviderClient;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class FeignError implements FeignProviderClient {
+    @Override
+    public Collection<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public String index() {
+        return "服务器维护中";
+    }
+}
